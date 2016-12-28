cf754de51d7b4c3815aa5407c56de07be4f4dab1
Metin Dagcilar <md3g11@soton.ac.uk>
2016-12-28 23:56:30 +0000

    retailSales.js line chart and title
    init commit for retailSales.js
    line chart and title

---
+++ retailSales.js
@@ -0,0 +1,100 @@
+/**
+ * storyline.js
+ *
+ * 
+ */
+
+
+
+var margin = {top: 30, right: 50, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+var parseDate = d3.time.format("%b-%y").parse;
+
+var x = d3.time.scale().range([0, width]);
+var y0 = d3.scale.linear().range([height, 0]);
+var y1 = d3.scale.linear().range([height, 0]);
+
+var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(5);
+
+var yAxisLeft = d3.svg.axis().scale(y0)
+  .orient("left").ticks(5);
+
+var yAxisRight = d3.svg.axis().scale(y1)
+  .orient("right").ticks(5); 
+
+var valueline = d3.svg.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y0(d.average_weekly_retailing); });
+  
+var valueline2 = d3.svg.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y1(d.average_weekly_internet_retailing); });
+  
+var svg = d3.select("body")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("resources/retail_sales_2011.csv", function(error, data) {
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.average_weekly_retailing = +d.average_weekly_retailing;
+    d.average_weekly_internet_retailing = +d.average_weekly_internet_retailing;
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { 
+    return d.date; }));
+  y0.domain([0, d3.max(data, function(d) {
+    return Math.max(d.average_weekly_retailing); })]); 
+  y1.domain([0, d3.max(data, function(d) { 
+    return Math.max(d.average_weekly_internet_retailing); })]);
+
+
+  svg.append("path")    // Add the valueline path.
+    .attr("class", "line")
+    .attr("d", valueline(data));
+
+  svg.append("path")    // Add the valueline2 path.
+    .attr("class", "line")
+    .style("stroke", "red")
+    .attr("d", valueline2(data));
+
+  svg.append("g")     // Add the X Axis
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  svg.append("g")     // Add the Y Axis
+    .attr("class", "y axis")
+    .call(yAxisLeft);
+
+  svg.append("g")     // Add the Y Axis
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + width + ", 0)")
+    .style("fill", "red")
+    .call(yAxisRight);
+
+
+  svg.append("text")
+    .attr("transform", "translate(" + (width/2) + "," + height/1.2 + ")")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "start")
+    .style("fill", "steelblue")
+    .text("Average Weekly Total Retailing");
+
+  svg.append("text")
+    .attr("transform", "translate(" + (width/2) + "," + height/1.38 + ")")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "start")
+    .style("fill", "red")
+    .text("Average Weekly Internet Retailing");
+
+
+});
